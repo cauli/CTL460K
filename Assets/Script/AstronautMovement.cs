5a089c0c54b682413c0a4c7de3c47e640f72c186
Cauli Tomaz <caulitomaz@gmail.com>
2015-01-24 00:09:41 -0200

    Added inventory Manager, game manager

--- Assets/Script/AstronautMovement.cs
+++ Assets/Script/AstronautMovement.cs
@@ -3,19 +3,11 @@ using System.Collections;
 
 public class AstronautMovement : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 		Debug.Log ("Start");
-		//iTween.MoveTo(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
 	}
 
-	void MoveTo() {
-		//iTween.MoveTo(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
-	}
-	// Update is called once per frame
 	void Update () {
-
-
 		if (Input.GetButtonDown ("Fire1")) {
 
 			Vector3 mousePos2D = Input.mousePosition;
---
+++ Assets/Script/InventoryManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryManager : MonoBehaviour {
+
+	Item[] currentItems;
+	int maxItems = 3;
+
+	void Start () 
+	{
+
+	}
+	
+	bool InsertItem (Item item) 
+	{
+
+	}
+
+	bool CombineItems (Item item1, Item item2)
+	{
+
+	}
+
+	void DropItem (Item item)
+	{
+
+	}
+
+	private bool canAddItem()
+	{
+		return currentItems.Length < maxItems;
+	}
+
+	void Update ()
+	{
+	
+	}
+}
