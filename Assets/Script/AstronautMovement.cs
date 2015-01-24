906a94c67fdcf43fba2402be2b7f788628fd3bf4
Cauli Tomaz <caulitomaz@gmail.com>
2015-01-23 22:55:20 -0200

    added basic item and itween

---
+++ Assets/Script/AstronautMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstronautMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+	}
+
+	void MoveTo() {
+		iTween.MoveTo(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.rigidbody2D.AddTorque(50);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
