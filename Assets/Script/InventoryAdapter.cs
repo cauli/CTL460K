0664d2244e74f0d6c3f1ca4b38f39eb626e4f1a8
Cauli Tomaz <caulitomaz@gmail.com>
2015-01-24 01:17:31 -0200

    Started ui

---
+++ Assets/Script/InventoryAdapter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryAdapter : MonoBehaviour {
+
+	public InventoryManager currentInventory;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
